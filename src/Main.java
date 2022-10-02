9bf9091ff9811406ae9f0b05a35b5335b441c209
Sozonchik <den.sozon@mail.ru>
2022-10-02 18:33:51 +0500

    Сделал 1.4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Домашнее задание 1. Циклы.
+        System.out.println("Задание 1.1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("Задание 1.2");
+for (int i = 10; i >0; i--) {
+    System.out.println(i);
+}
+
+        System.out.println("Задание 1.3");
+for (int i = 0; i <=17; i+=2) {
+            System.out.println(i);
+        }
+
+        System.out.println("Задание 1.4");
+for (int i = 10; i >= -10; i--) {
+    System.out.println(i);
+}
+
+
     }
 }
