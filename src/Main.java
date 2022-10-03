1240cc4b37e6c321ca24d34540fc2f5e7ae4ffe3
Sozonchik <den.sozon@mail.ru>
2022-10-03 19:35:20 +0500

    Сделал 2.3

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,28 @@ for (int i = 0; i <=17; i+=2) {
 for (int i = 10; i >= -10; i--) {
     System.out.println(i);
 }
+        //Домашнее задание 2
+        System.out.println("Задание 2.1");
+for (int y = 1904; y <= 2096; y+=4) {
+    System.out.println(y + " год является високосным");
+}
 
+        System.out.println("Задание 2.2");
+for (int i = 7; i <= 98; i+=7) {
+    System.out.println(i);
+}
+
+        System.out.println("Задание 2.3");
+for (int d = 1; d <= 512; d*=2) {
+    System.out.println(d);
+}
+//Домашнее задание 3
+        System.out.println("Задание 3.1");
+int money = 29_000;
+int month = 1;
+for (money = 29_000; month <= 12; money+=29000, month++) {
+    System.out.println(money + " месяц " + month);
+}
 
     }
 }
